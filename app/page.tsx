c4b4332650248379de925cca35ff8b085bfd4997
Maaz Shaikh <maaz.z.shaikh@gmail.com>
2025-09-17 23:51:48 +0530

    bug fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1>hello</h1>;
+  return (
+    <h1 className="flex justify-center items-center w-full h-screen">
+      Welcome to Edviron Payment Dashboard!
+    </h1>
+  );
 }
